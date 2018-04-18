d305f38ddd736e7bb89e0fe9f56d5f4410b9fd4d
ldy <ldy@ldydeiMac.local>
2018-04-18 20:21:35 +0800

    login

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 var express = require("express")
 var app = express()
 var router = require("./router/router.js")
+var session = require('express-session');
+
+//使用session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
 app.set("view engine","ejs")
 app.use(express.static("./public"))
 
 app.get("/",router.showIndex)
 
 app.get("/regist",router.register)
-
 app.post("/doRegist",router.doRegist)
 
+app.get("/showlogin",router.showlogin)
+app.post("/login",router.login)
+
+app.get("/showAvatar",router.showAvatar)
+
+app.post("/avatar",router.avatar)
 
 app.listen(3000)
--- router/router.js
+++ router/router.js
@@ -1,18 +1,102 @@
 var formidable = require('formidable')
+var md5 = require('../model/md5.js')
 var util = require('util')
+var db = require('../model/db.js')
 
 exports.showIndex = (req,res,next)=>{
-	res.render("index")
+
+	var renderData = {
+		login:req.session.login=="1",
+		 username:req.session.username,
+		 active:"首页"
+	}
+	res.render("index",renderData)
+
 }
+
 exports.register = (req,res,next)=>{
-	res.render("regist")
+	var renderData = {
+		login:req.session.login=="1",
+		 username:req.session.username,
+		 active:""
+	}
+	res.render("regist",renderData)
 }
 
 exports.doRegist = (req,res,next)=>{
 	var form = new formidable.IncomingForm();
 	form.parse(req, function(err, fields, files) {
 	res.writeHead(200, {'content-type': 'text/plain'});
-	console.log("xxxxxxxxxxxxxxxxxxxxxx");
-	res.end(util.inspect({fields: fields,files: files}));
+	var username = fields.username
+	var password = fields.password
+	password = md5(password)
+	db.find("users",{username:username},(err,response)=>{
+		if (err) {
+			res.end("-3")
+		}else if(response.length==0){
+			req.session.login = "1"
+			req.session.username = username
+			// req.seesion.password = password
+			db.insertOne("users",{username:username,password:password},(err,response)=>{
+
+			})
+			res.end("1")
+		}else{
+
+			res.end("-1")}
+	})
+	console.log(fields.username,fields.password,password);
+	// res.end(util.inspect({fields: fields,files: files}));
 })
+}
+
+exports.showlogin = (req,res,next)=>{
+	var renderData = {
+		login:req.session.login=="1",
+		 username:req.session.username,
+		 active:""
+	}
+	res.render("login",renderData)
+}
+
+exports.login = (req,res,next)=>{
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files) {
+		res.writeHead(200, {'content-type': 'text/plain'});
+		var username = fields.username
+		var password = fields.password
+		password = md5(password)
+		db.find("users",{username:username},(err,response)=>{
+			console.log("find:",username,response);
+			var user = response[0]
+
+			if (err) {
+				res.end("-3")
+			}else if(response.length==1){
+				if (user.password!=password) {
+					res.end("-2")
+				}
+				req.session.login = "1"
+				req.session.username = username
+
+				res.end("1")
+			}else{
+				res.end("-1")
+			}
+		})
+	})
+}
+// 头像
+exports.showAvatar = (req,res,next)=>{
+	var renderData = {
+		login:req.session.login=="1",
+		 username:req.session.username,
+		 active:""
+	}
+	res.render("avatar",renderData)
+}
+
+exports.avatar = (req,res,next)=>{
+
+	
 }
